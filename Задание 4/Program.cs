71576ed5a9a3807f30accb166b316dcf52e42d0e
Ivan <63220196+Ivan-Surkov@users.noreply.github.com>
2020-06-15 00:13:05 +0500

    commit

---
+++ Задание 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace zadacha 4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] n = new byte[100];
+            n[0] = 1;
+            n[1] = 1;
+            byte b;
+            for (int p = 0; p < 200; p++)
+            {
+                b = 0;
+                for (int i = 1; i <= n[0]; i++)
+                {
+                    b += Convert.ToByte(n[i] * 2);
+                    n[i] = Convert.ToByte(b % 10);
+                    b /= 10;
+                }
+                if (b > 0)
+                {
+                    n[0]++;
+                    n[n[0]] = b;
+                }
+            }
+            for (int i = n[0]; i >= 1; i--)
+            {
+                Console.Write(n[i]);
+            }
+        }
+    }
+}
