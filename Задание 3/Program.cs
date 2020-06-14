875cb14606a50a027b39ead974b8ea38bb919258
Ivan <63220196+Ivan-Surkov@users.noreply.github.com>
2020-06-15 00:10:19 +0500

    commit

---
+++ Задание 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Zadacha3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите x: ");
+            float x;
+            while (!float.TryParse(Console.ReadLine(), out x))
+            {
+                Console.Write("Вы ввели неверное значение \n" + "Введите x: ");
+            }
+
+            Console.Write("Введите y: ");
+            float y;
+            while (!float.TryParse(Console.ReadLine(), out y))
+                Console.Write("Вы ввели неверное значение \n" + "Введите y: ");
+
+            Console.WriteLine(Math.Abs(y) >= 1 - Math.Abs(x) ? "Точка не принадлежит заданной области!" : "Точка принадлежит заданной области!");
+        }
+    }
+}
